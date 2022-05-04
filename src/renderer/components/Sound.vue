1238cd022c4d13f5da8bbc1f1f7b566959fb4b46
Cody Bontecou <bontecouc@gmail.com>
2022-05-03 19:48:53 -1000

    feat: :sparkles: Integrated useIntervalFn composable from vueuse
    Profides pause, resume, isActive states for easy sound control.

--- src/renderer/components/Sound.vue
+++ src/renderer/components/Sound.vue
@@ -1,10 +1,22 @@
 <script setup>
+import { ref } from 'vue'
+import { useIntervalFn } from '@vueuse/core'
 import { useSound } from '@vueuse/sound'
 import buttonSfx from '../assets/error.wav'
 
 const { play } = useSound(buttonSfx)
+
+const interval = ref(4000)
+const { pause, resume, isActive } = useIntervalFn(() => {
+  play()
+}, interval)
 </script>
 
 <template>
-  <button @click="play">Play a sound</button>
+  <p>
+    interval:
+    <input v-model="interval" type="number" placeholder="interval" />
+  </p>
+  <button v-if="isActive" class="orange" @click="pause">Pause</button>
+  <button v-if="!isActive" @click="resume">Resume</button>
 </template>
