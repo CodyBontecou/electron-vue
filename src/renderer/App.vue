e72e46b5d82dff98f73af05b31260ceb4cb6bfe6
Cody Bontecou <bontecouc@gmail.com>
2022-05-03 19:07:06 -1000

    feat: :sparkles: vueuse/sound dependency added
    vueuse/sound provides easy-to-use composables that make playing sounds from the web app easily.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <hello></hello>
+    <hello />
+    <sound />
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import Hello from './components/Hello.vue'
+import Sound from './components/Sound.vue'
 import { ipcRenderer } from '@/electron'
 
 export default defineComponent({
   name: 'app',
   components: {
     Hello,
+    Sound,
   },
   setup() {
     ipcRenderer.send('message', 'Hello from App.vue!')
---
+++ src/renderer/components/Sound.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useSound } from '@vueuse/sound'
+import buttonSfx from '../assets/error.wav'
+
+const { play } = useSound(buttonSfx)
+</script>
+
+<template>
+  <button @click="play">Play a sound</button>
+</template>
