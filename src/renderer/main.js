4d3a2134e152d532c7c24b96bd8c3e973f276ff8
Cody Bontecou <bontecouc@gmail.com>
2022-06-01 13:42:16 -1000

    Brought in tailwindcss

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,6 +2,7 @@ import devtools from '@vue/devtools'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import './assets/css/index.css'
 
 createApp(App).use(createPinia()).mount('#app')
 
